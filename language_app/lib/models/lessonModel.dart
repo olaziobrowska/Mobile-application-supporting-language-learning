ad12ef7fbfba72f76ed250aa819de835e4794e29
olaziobrowska <olaziobrowska1@gmail.com>
2021-04-11 23:36:06 +0200

    add note model, add uid to flashcard, lesson models

--- language_app/lib/models/lessonModel.dart
+++ language_app/lib/models/lessonModel.dart
@@ -4,17 +4,19 @@ import 'package:language_app/models/userModel.dart';
 final String pointsFirebaseColumn = "Points";
 final String nameFirebaseColumn = "Name";
 final String dateFirebaseColumn = "Date";
+final String uidFirebaseColumn = "Uid";
 
 class LessonModel {
   String id;
   String name;
   DateTime date;
+  String uid;
 
-  LessonModel._({this.id, this.name, this.date});
+  LessonModel._({this.id, this.name, this.date, this.uid});
 
   static LessonModel newLesson({UserModel userModel, String name}) {
     if (userModel == null) return null;
-    return LessonModel._(id: userModel.id, name: name, date: DateTime.now());
+    return LessonModel._( uid: userModel.id, name: name, date: DateTime.now());
   }
 
   static LessonModel newFromFireStore(DocumentSnapshot document) {
@@ -22,7 +24,8 @@ class LessonModel {
     return LessonModel._(
         id: document.documentID,
         name: map[nameFirebaseColumn],
-        date: map[dateFirebaseColumn]);
+        date: map[dateFirebaseColumn],
+        uid: map[uidFirebaseColumn]);
   }
 
   static Map<String, dynamic> toMap(LessonModel lessonModel) {
@@ -30,6 +33,7 @@ class LessonModel {
     Map<String, dynamic> output = Map();
     if (lessonModel.name != null) output[nameFirebaseColumn] = lessonModel.name;
     if (lessonModel.date != null) output[dateFirebaseColumn] = lessonModel.date;
+    if (lessonModel.uid != null) output[uidFirebaseColumn] = lessonModel.uid;
     return output;
   }
 }
