510308c847f4e95d6facab91ba4e11f1394b57c5
ITermiTI <pawliko@poczta.onet.eu>
2021-05-03 19:20:43 +0200

    Auth screens styling and model improvements

--- language_app/lib/components/common/commonComponents.dart
+++ language_app/lib/components/common/commonComponents.dart
@@ -10,7 +10,7 @@ InputDecoration buildInputDecoration(String hintText, IconData icon) {
   return InputDecoration(
     prefixIcon: Icon(icon, color: iconColor),
     hintText: hintText,
-    contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+    contentPadding: EdgeInsets.fromLTRB(20.0, 7.5, 20.0, 7.5),
     border: OutlineInputBorder(borderRadius: BorderRadius.circular(5.0)),
   );
 }
