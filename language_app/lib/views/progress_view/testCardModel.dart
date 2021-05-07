da135fe2c0308955bcfda223739d1224a8152e52
Snowflake <sniezek.adrian97@gmail.com>
2021-05-07 13:05:55 +0200

    progressView + testCard + testCardModel

--- language_app/lib/views/progress_view/testCardModel.dart
+++ language_app/lib/views/progress_view/testCardModel.dart
@@ -1,8 +1,7 @@
-import 'package:intl/intl.dart';
-import 'package:language_app/view_models/flashcards/flashcardTestResultViewModel.dart';
+import 'package:flutter/material.dart';
 
 class TestCardModel {
-  String id;
+  int id;
   String name;
   int percentage;
 
@@ -11,10 +10,4 @@ class TestCardModel {
         this.name,
         this.percentage
   );
-
-  static TestCardModel fromTestResult(FlashcardTestResultViewModel resultViewModel){
-    final format = new DateFormat('dd-MM-yyyy hh:mm');
-    var name = format.format(resultViewModel.date.toDate());
-    return TestCardModel(resultViewModel.points.toString() + " / " + resultViewModel.maxPoints.toString(), name, resultViewModel.points * 100 ~/ resultViewModel.maxPoints);
-  }
 }
