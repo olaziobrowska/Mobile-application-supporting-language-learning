c0e83cd1d5cfc2199bc4e8e0d190a7f95c8edd42
Snowflake <sniezek.adrian97@gmail.com>
2021-05-07 13:05:55 +0200

    progressView + testCard + testCardModel

---
+++ language_app/lib/views/progress_view/testCardModel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TestCardModel {
+  int id;
+  String name;
+  int percentage;
+
+  TestCardModel(
+      this.id,
+        this.name,
+        this.percentage
+  );
+}
