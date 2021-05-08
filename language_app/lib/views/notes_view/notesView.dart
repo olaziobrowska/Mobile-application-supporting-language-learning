1dd7e4c8d5cc6ee4d31451b2df5cfccfb5a1322f
olaziobrowska <olaziobrowska1@gmail.com>
2021-05-08 22:32:45 +0200

    transfer variables to notesStyle, add MaterialButton

--- language_app/lib/views/notes_view/notesView.dart
+++ language_app/lib/views/notes_view/notesView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:language_app/components/appbar/appbar.dart';
 import 'package:language_app/components/appbar/code/appbarCode.dart';
 import 'package:language_app/components/common/commonComponents.dart';
@@ -23,6 +24,7 @@ class NotesView extends StatefulWidget {
 
 class _NotesViewState extends State<NotesView> {
   final NotesViewModel _notesViewModel = NotesViewModel.instance;
+  String lessonId = "1";
 
   @override
   Widget build(BuildContext context) {
@@ -48,9 +50,41 @@ class _NotesViewState extends State<NotesView> {
           child: Center(
               child: Container(
                   child: Form(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [NotesViewPadding(widgetList, logoPath)]),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: height1),
+                    SizedBox(height: imageHeight, child: InsertImage(logoPath)),
+                    SizedBox(height: height1),
+                    Text("Lesson " + lessonId, style: textStyle),
+                    widgetList[0],
+                    SizedBox(height: height1),
+                    TextField(
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        decoration: InputDecoration(
+                            labelText: 'Your note',
+                            helperText:
+                                'Enter the text and then accept with the submit button',
+                            hintText: 'Please enter notes',
+                            prefixIcon: const Icon(FontAwesomeIcons.bookOpen,
+                                color: Colors.teal),
+                            border: new OutlineInputBorder(
+                                borderSide:
+                                    new BorderSide(color: Colors.teal)))),
+                    SizedBox(height: height1),
+                    widgetList[1],
+                    SizedBox(height: height2),
+                    MaterialButtonComponent()
+                  ],
+                ),
+              )
+            ]),
           ))),
         ),
       ),
