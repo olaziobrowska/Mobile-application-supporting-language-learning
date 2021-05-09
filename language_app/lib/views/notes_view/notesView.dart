66bbe14eaecb2f34165bb0273cd3b7490573444a
olaziobrowska <olaziobrowska1@gmail.com>
2021-05-09 16:36:12 +0200

    add logic for notes and add file picker to pubspec

--- language_app/lib/views/notes_view/notesView.dart
+++ language_app/lib/views/notes_view/notesView.dart
@@ -4,17 +4,19 @@ import 'package:language_app/components/appbar/appbar.dart';
 import 'package:language_app/components/appbar/code/appbarCode.dart';
 import 'package:language_app/components/common/commonComponents.dart';
 import 'package:language_app/components/drawer/drawer.dart';
+import 'package:language_app/models/lessonModel.dart';
 import 'package:language_app/utils/global_const/globalLayout.dart';
-import 'package:language_app/utils/routes/routes.dart';
 import 'package:language_app/views/notes_view/notesModel.dart';
 import 'package:provider/provider.dart';
 
 import 'notesStyle.dart';
+//TODO załaczniki
 
 class NotesView extends StatefulWidget {
-  final NavigationService _navigationService = locator<NavigationService>();
-
-  NotesView({Key key, this.title}) : super(key: key);
+  NotesView(LessonModel selectedModel, {Key key, this.title})
+      : super(key: key) {
+    NotesViewModel.instance.selectedLesson = selectedModel;
+  }
 
   final String title;
 
@@ -24,68 +26,90 @@ class NotesView extends StatefulWidget {
 
 class _NotesViewState extends State<NotesView> {
   final NotesViewModel _notesViewModel = NotesViewModel.instance;
-  String lessonId = "1";
+
+  @override
+  void initState() {
+    super.initState();
+    _notesViewModel.getNotes();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final inputWord = TextInputComponent("Title", false, null);
     final logoPath = "assets/images/paper1.png";
-    final submitButton = OnPressButton("Submit", () {
-      widget._navigationService.navigateTo("homeView", 1);
+    final submitButton = OnPressButton("Submit", () async {
+      await _notesViewModel.submitNotes();
     }, context);
 
     List<Widget> widgetList = [
-      inputWord,
       submitButton,
     ];
 
     return ChangeNotifierProvider.value(
       value: _notesViewModel,
-      child: Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size.fromHeight(50),
-            child: MainAppBar(code: AppbarCode.New(notesTitle))),
-        drawer: MainDrawer(),
-        body: SingleChildScrollView(
-          child: Center(
-              child: Container(
-                  child: Form(
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(height: height1),
-                    SizedBox(height: imageHeight, child: InsertImage(logoPath)),
-                    SizedBox(height: height1),
-                    Text("Lesson " + lessonId, style: textStyle),
-                    widgetList[0],
-                    SizedBox(height: height1),
-                    TextField(
-                        keyboardType: TextInputType.multiline,
-                        maxLines: null,
-                        decoration: InputDecoration(
-                            labelText: 'Your note',
-                            helperText:
-                                'Enter the text and then accept with the submit button',
-                            hintText: 'Please enter notes',
-                            prefixIcon: const Icon(FontAwesomeIcons.bookOpen,
-                                color: Colors.teal),
-                            border: new OutlineInputBorder(
-                                borderSide:
-                                    new BorderSide(color: Colors.teal)))),
-                    SizedBox(height: height1),
-                    widgetList[1],
-                    SizedBox(height: height2),
-                    MaterialButtonComponent()
-                  ],
-                ),
-              )
-            ]),
-          ))),
+      child: Consumer<NotesViewModel>(
+        builder: (context, viewModel, child) => Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size.fromHeight(50),
+              child: MainAppBar(code: AppbarCode.New(notesTitle))),
+          drawer: MainDrawer(),
+          body: SingleChildScrollView(
+            child: Center(
+                child: Container(
+                    child: Form(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(height: height1),
+                          SizedBox(
+                              height: imageHeight,
+                              child: InsertImage(logoPath)),
+                          SizedBox(height: height1),
+                          Text(_notesViewModel.selectedLesson.name,
+                              style: textStyle),
+                          TextInputComponent2(
+                              "Title",
+                              false,
+                              null, (val) {
+                            _notesViewModel.note.title = val;
+                          },
+                              TextEditingController(
+                                  text: _notesViewModel.note.title)),
+                          SizedBox(height: height1),
+                          TextField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              onChanged: (val) {
+                                _notesViewModel.note.content[0] = val;
+                              },
+                              controller: TextEditingController(
+                                  text: _notesViewModel.note.content[0]),
+                              decoration: InputDecoration(
+                                  labelText: 'Your note',
+                                  helperText:
+                                      'Enter the text and then accept with the submit button',
+                                  hintText: 'Please enter notes',
+                                  prefixIcon: const Icon(
+                                      FontAwesomeIcons.bookOpen,
+                                      color: Colors.teal),
+                                  border: new OutlineInputBorder(
+                                      borderSide:
+                                          new BorderSide(color: Colors.teal)))),
+                          SizedBox(height: height1),
+                          widgetList[0],
+                          SizedBox(height: height2),
+                          MaterialButtonComponent()
+                        ],
+                      ),
+                    )
+                  ]),
+            ))),
+          ),
         ),
       ),
     );
