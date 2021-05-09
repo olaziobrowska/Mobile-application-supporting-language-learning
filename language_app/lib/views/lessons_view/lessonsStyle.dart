b40371371ba20add7c477378e081d9add225567f
olaziobrowska <olaziobrowska1@gmail.com>
2021-05-09 12:39:17 +0200

    remove entries in lessonStyle

--- language_app/lib/views/lessons_view/lessonsStyle.dart
+++ language_app/lib/views/lessons_view/lessonsStyle.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-final List<String> entries = <String>['1', '2', '3'];
 final List<int> colorCodes = <int>[300, 200, 100];
 final double height1 = 10.0;
 final double imageHeight = 250.0;
