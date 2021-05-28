4bb4a0a7a8816798cabe8d32e3bfa24e1d00eb1e
Snowflake <sniezek.adrian97@gmail.com>
2021-05-28 13:56:42 +0200

    progressView updated and fixed

--- language_app/lib/views/progress_view/progressView.dart
+++ language_app/lib/views/progress_view/progressView.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:language_app/services/testService.dart';
-import 'package:language_app/utils/routes/routes.dart';
+import 'package:language_app/components/appbar/appbar.dart';
+import 'package:language_app/components/appbar/code/appbarCode.dart';
+import 'package:language_app/components/drawer/drawer.dart';
+import 'package:language_app/utils/global_const/globalLayout.dart';
+import 'package:language_app/views/progress_view/progressModel.dart';
+import 'package:provider/provider.dart';
 import 'testCard.dart';
 import 'testCardModel.dart';
 
 class ProgressView extends StatefulWidget {
-
+  ProgressView({Key key, this.title}) : super(key: key);
+  final String title;
   @override
   _ProgressViewState createState() => _ProgressViewState();
 }
 
 class _ProgressViewState extends State<ProgressView> {
+  final ProgressViewModel _progressViewModel = ProgressViewModel.instance;
   String searchValue = "";
-  bool loading = true;
-
   TextEditingController editingController = TextEditingController();
 
-  var items = List.of({new TestCardModel("ssda", "Raz", 51)});
+  var items = List<TestCardModel>();
   var filteredItems = List<TestCardModel>();
-  List<String> selectedCountList = [];
 
   @override
   void initState() {
     super.initState();
-    List<TestCardModel> models = [];
-    TestService.instance.getUsersTestResults().then((value) {
-      value.forEach((element) {
-        models.add(TestCardModel.fromTestResult(element));
-      });
-      updateItems(models);
-    }
-    );
+    update();
   }
 
-  void updateItems(List<TestCardModel> models){
-    if(models.isEmpty) models.add(new TestCardModel("ssda", "Raz", 51));
+  void update() async {
+    await _progressViewModel.getTestsResults();
     setState(() {
-      items = models;
+      items = _progressViewModel.userTestCardModels;
     });
   }
 
@@ -52,85 +48,100 @@ class _ProgressViewState extends State<ProgressView> {
         .where((item) =>
             item.name.toLowerCase().contains(searchValue.toLowerCase()))
         .toList();
-    filteredItems = filteredItems.where((item) {
-      if (selectedCountList.isEmpty) {
-        return true;
-      }
-      return false;
-    }).toList();
 
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("Progress"),
-        ),
-        body: WillPopScope(
-          child: Container(
-            margin: new EdgeInsets.symmetric(horizontal: 8.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: [
-                    Expanded(
-                      flex: 7,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextField(
-                          onChanged: (value) {
-                            filterSearchResults(value);
-                          },
-                          controller: editingController,
-                          style: TextStyle(fontSize: 16.0, color: Colors.black),
-                          decoration: InputDecoration(
-                              fillColor: Colors.grey.withOpacity(0.2),
-                              filled: true,
-                              hintText: "Search",
-                              contentPadding: const EdgeInsets.symmetric(
-                                vertical: 8.0,
-                              ),
-                              prefixIcon: Icon(
-                                Icons.search,
-                                color: Colors.blue[300],
-                              ),
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide.none,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(20.0)))),
+    return ChangeNotifierProvider.value(
+      value: _progressViewModel,
+      child: Consumer<ProgressViewModel>(
+        builder: (context, viewModel, child) => Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size.fromHeight(50),
+              child: MainAppBar(code: AppbarCode.New(resultsTitle))),
+          drawer: MainDrawer(),
+          body: items.length > 0
+                  ? WillPopScope(
+                      child: Container(
+                        margin: new EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              children: [
+                                Expanded(
+                                  flex: 7,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextField(
+                                      onChanged: (value) {
+                                        filterSearchResults(value);
+                                      },
+                                      controller: editingController,
+                                      style: TextStyle(
+                                          fontSize: 16.0, color: Colors.black),
+                                      decoration: InputDecoration(
+                                          fillColor: Colors.grey.withOpacity(0.2),
+                                          filled: true,
+                                          hintText: "Search",
+                                          contentPadding: const EdgeInsets.symmetric(
+                                            vertical: 8.0,
+                                          ),
+                                          prefixIcon: Icon(
+                                            Icons.search,
+                                            color: Colors.blue[300],
+                                          ),
+                                          border: OutlineInputBorder(
+                                              borderSide: BorderSide.none,
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(20.0)))),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Expanded(
+                                child: CustomScrollView(slivers: <Widget>[
+                              SliverGrid(
+                                  gridDelegate:
+                                      SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount: 1,
+                                          childAspectRatio: 12 / 3,
+                                          crossAxisSpacing: 8.0,
+                                          mainAxisSpacing: 8.0),
+                                  delegate: SliverChildBuilderDelegate(
+                                    (BuildContext context, int index) {
+                                      return GestureDetector(
+                                        child: new Card(
+                                            elevation: 5,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.circular(15),
+                                            ),
+                                            child: TestCard(
+                                                testCard: filteredItems[index])),
+                                      );
+                                    },
+                                    childCount: filteredItems.length,
+                                  )),
+                            ])),
+                          ],
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Expanded(
-                    child: CustomScrollView(slivers: <Widget>[
-                  SliverGrid(
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 1,
-                          childAspectRatio: 12 / 3,
-                          crossAxisSpacing: 8.0,
-                          mainAxisSpacing: 8.0),
-                      delegate: SliverChildBuilderDelegate(
-                        (BuildContext context, int index) {
-                          return GestureDetector(
-                            child: new Card(
-                                elevation: 5,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15),
-                                ),
-                                child:
-                                    TestCard(testCard: filteredItems[index])),
-                            onTap: () async {
-                              // TODO
-                              // await widget._navigationService
-                              //     .navigateTo('REPLACE_ME_WITH_TEST_VIEW_URL', filteredItems[index].id);
-                            },
-                          );
-                        },
-                        childCount: filteredItems.length,
-                      )),
-                ])),
-              ],
-            ),
-          ),
-        ));
+                    )
+                  : emptyResultsContainer())
+        ),
+      );
   }
+}
+
+Container emptyResultsContainer() {
+  return Container(
+    child: Column(mainAxisSize: MainAxisSize.min, children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+            child: Text("No test were taken yet")
+          )
+        ],
+      ),
+    ]),
+  );
 }
